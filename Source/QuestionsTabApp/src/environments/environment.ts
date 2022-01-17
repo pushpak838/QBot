53fef45e8c7ad3ad6512ede34d9a0f3f0820f1f6
pushpak838 <64265579+pushpak838@users.noreply.github.com>
2022-01-17 18:13:33 +0530

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://teamchatbot12.azurewebsites.net/api/Request/",
+    selfUrl: "https://teamchatbot12-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "59304d84-3df0-45e2-8834-de4654ae8758",
+        clientId: "da7e3d76-17dc-4e09-b5d1-b4903ae3c7a6",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
